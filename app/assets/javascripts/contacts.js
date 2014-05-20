a95f62c77ad248a3d58629613b061175cdf47d8a
Delmer Reed <delmer814@yahoo.com>
2014-05-20 11:52:01 -0700

    style and stuff

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,3 +1,77 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// Waiting for the DOM to finish loading
+$(document).ready(function(){
+ 
+ 
+  var contacts = [];
+  var count = 0;
+ 
+ 
+  
+  var deleteContact = function(event){
+    console.log("Delete", this);
+    $(this).parents(".contact").remove();
+  };
+  
+  $("#contacts").on("click", ".delete", deleteContact);
+ 
+ 
+  var addContact = function(newContact){
+    var contactString = ["<div id='", newContact.id, "' class='contact'>",
+                               "<div>",
+                              "<img src='", newContact.imgUrl, "' class='contact-img'>",
+                            "</div>",
+                            "<div class='contact-item'>", newContact.name, "</div>",
+                            "<div class='contact-item'>", newContact.email, "</div>",
+                            "<div class='contact-item'>", newContact.number, "</div>",
+          
+                            "<div class='contact-actions'>",
+                            
+                              "<span class='delete btn btn-action'>Delete</span>",
+                            "</div>",
+                          "</div>"].join("");
+ 
+    console.log(contactString);
+ 
+    $("#contacts").append(contactString);
+  
+  };
+ 
+  // Now we need to watch for a submit 
+  //  event on the form
+  $("#new_contact").submit( function(event){
+    // Prevent the page from reloading
+    event.preventDefault();
+ 
+    //console.log(this);
+ 
+    var name = $("#contact_name").val();
+    var email = $("#contact_email").val();
+    var number = $("#contact_number").val();
+    var imgUrl =  $("#contact_img_url").val();
+ 
+    console.log(name, email, number, imgUrl);
+ 
+    // Reset the form
+    this.reset();
+ 
+ 
+    // Keeping track of new contacts
+    var newContact = { id: count,
+                       name: name,
+                       email: email,
+                       number: number,
+                       imgUrl: imgUrl };
+    count += 1;
+ 
+ 
+    contacts.push(newContact);
+ 
+    // Call a function to add our contact to 
+    //  the page.
+    addContact(newContact);
+ 
+  });
+ 
+ 
+ 
+});
